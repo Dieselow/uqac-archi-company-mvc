64fb350136e7e26ed35f554c381493c070f2bd83
louisdumont <louisdumontbarbosa@protonmail.com>
2021-11-15 19:50:50 -0500

    finally something working with startup

---
+++ archi-company-mvc/Constants/Roles.cs
@@ -0,0 +1,10 @@
+namespace archi_company_mvc.Constants
+{
+    public enum Roles
+    {
+        Admin,
+        Patient,
+        Caregiver,
+        Secretary
+    }
+}
--- archi-company-mvc/Startup.cs
+++ archi-company-mvc/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,10 @@ namespace archi_company_mvc
 
             services.AddDbContext<DatabaseContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DatabaseContext")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DatabaseContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
